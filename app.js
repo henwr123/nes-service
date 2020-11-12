7aa5c944e181168436e26c94363c79bc4cee607f
William Hendrick <william.hendrick@gmail.com>
2020-11-12 10:11:08 -0600

    Adding mock support

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const games = require('./mock-games');
 
 
 const app = express();
-const port = 3333;
+const port = 3000;
+const catalog = games;
 
 app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+
+/**
+ * Get list of games with filtering
+ */
+app.get('/games', (req, res) => {
+    res.send(catalog);
+});
 
 app.listen(port, () => {
     console.log(`Welcome to the NES Service - service is listening on port ${port}!`);
---
+++ mock-games.js
@@ -0,0 +1,19 @@
+const CATALOG = [
+    {
+        id: 'NES-WO-USA',
+        name: 'The 3-D Battles of WorldRunner',
+        developer: 'Square',
+        publisher: 'Acclaim Entertainment',
+        region: 'NTSC (N. America)',
+        releaseDate: new Date('September 1987'),
+        category: 'Action & Adventure',
+        esrb: 'E',
+        players: '',
+        upc: '021481102014',
+        board: 'NES-UNROM',
+        sortable: '3-D Battles of WorldRunner',
+        owned: false
+    }
+];
+
+module.exports = CATALOG;
