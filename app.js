380c478f464944fff2f06770930085c986d8d23b
William Hendrick <william.hendrick@gmail.com>
2021-11-16 17:08:55 -0600

    Remove bodyParser and logging in DAO

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const Promise = require('bluebird')
 const AppDAO = require('./dao')
 const CatalogRepository = require('./catalog_repository')
 const express = require('express');
-//const sqlite3 = require('sqlite3')
-const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
@@ -11,73 +9,9 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 const dao = new AppDAO('./games.db')
 const catalogRepo = new CatalogRepository(dao)
-
-
-
-// const db = new sqlite3.Database('./games.db', (err) => {
-//     if (err) {
-//         console.error('Error opening database: ${ err.message }')
-//     }
-// })
-
-
-
-// /**
-//  * Build the filtering parameters
-//  * @param {*} params - the req.query object 
-//  * @returns Filtering string
-//  */
-// function buildFilteringWhereClause(params) {
-
-//     let filter = ""
-
-//     Object.entries(params).forEach(entry => {
-
-//         let key = entry[0];
-//         let value = entry[1];
-
-//         // ignore the orderBy parameter
-//         if (key === 'orderBy' || key === 'orderDir') {
-//             return
-//         }
-
-//         if (filter === "") {
-//             filter = ' WHERE ' + key + ' LIKE "' + value + '" '
-//         } else {
-//             filter = filter + 'AND ' + key + ' LIKE "' + value + '" '
-//         }
-//     })
-
-//     return filter
-// }
-
-// /**
-//  * Build the OrderBy clause based upon the provided parameters
-//  * @param {*} params 
-//  * @param {*} def 
-//  * @returns 
-//  */
-// function buildSortingOrderBy(params, def = 'catalog_id') {
-
-//     let sort = ""
-
-//     if (params.orderBy !== undefined) {
-//         sort = " ORDER BY " + params.orderBy
-//     } else {
-//         sort = " ORDER BY " + def
-//     }
-
-//     // do not apply the order direction if a sorting property was not applied
-//     if (params.orderDir !== undefined && params.orderBy !== undefined) {
-//         sort = sort + " " + params.orderDir
-//     }
-//     return sort
-// }
 
 
 
@@ -116,9 +50,6 @@ app.get('/games/:catalog_id', (req, res) => {
  */
 app.get('/games', (req, res) => {
 
-    console.log(`🔎  request for games`);
-
-
     catalogRepo.getFiltered(req.query).then((cat) => {
 
         console.log(`✔️  Games - Found ${cat.length} records`)
@@ -131,7 +62,7 @@ app.get('/games', (req, res) => {
     }).catch((err) => {
 
         console.error(`❗  Games - Error - ${err.message}`)
-        res.status(400).json({ error: err.message }).end()
+        res.status(400).json({ message: `Something went wrong - No games found` }).end()
 
     })
 });
--- dao.js
+++ dao.js
@@ -20,7 +20,6 @@ class AppDAO {
         return new Promise((resolve, reject) => {
             this.db.run(sql, params, function (err) {
                 if (err) {
-                    console.log('Error running sql ' + sql)
                     console.log(err)
                     reject(err)
                 } else {
@@ -34,7 +33,6 @@ class AppDAO {
         return new Promise((resolve, reject) => {
             this.db.get(sql, params, (err, result) => {
                 if (err) {
-                    console.log('Error running sql: ' + sql)
                     console.log(err)
                     reject(err)
                 } else {
@@ -47,9 +45,7 @@ class AppDAO {
     all(sql, params = []) {
         return new Promise((resolve, reject) => {
             this.db.all(sql, params, (err, rows) => {
-                console.log(sql)
                 if (err) {
-                    console.log('Error running sql: ' + sql)
                     console.log(err)
                     reject(err)
                 } else {
