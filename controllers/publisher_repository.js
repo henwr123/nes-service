5abb55f0603d9cf60c2b4a420ca6e309e878a9b3
William Hendrick <william.hendrick@gmail.com>
2021-11-17 20:26:36 -0600

    create base repo class and change publisher

--- controllers/publisher_repository.js
+++ controllers/publisher_repository.js
@@ -1,65 +1,15 @@
 
-/**
- * Build the filtering parameters
- * @param {*} params - the req.query object 
- * @returns Filtering string
- */
- function buildFilteringWhereClause(params) {
-
-    let filter = ""
-
-    Object.entries(params).forEach(entry => {
-
-        let key = entry[0];
-        let value = entry[1];
-
-        // ignore the orderBy parameter
-        if (key === 'orderBy' || key === 'orderDir') {
-            return
-        }
-
-        if (filter === "") {
-            filter = ' WHERE ' + key + ' LIKE "' + value + '" '
-        } else {
-            filter = filter + 'AND ' + key + ' LIKE "' + value + '" '
-        }
-    })
-
-    return filter
-}
-
-/**
- * Build the OrderBy clause based upon the provided parameters
- * @param {*} params 
- * @param {*} def 
- * @returns 
- */
- function buildSortingOrderBy(params, def = 'publisher_id') {
-
-    let sort = ""
-
-    if (params.orderBy !== undefined) {
-        sort = " ORDER BY " + params.orderBy
-    } else {
-        sort = " ORDER BY " + def
-    }
-
-    // do not apply the order direction if a sorting property was not applied
-    if (params.orderDir !== undefined && params.orderBy !== undefined) {
-        sort = sort + " " + params.orderDir
-    }
-    return sort
-}
+const Repository = require('./repository')
 
 
-class PublisherRepository {
+class PublisherRepository extends Repository {
 
     /**
      * Create an instance of the Publisher Repository object for DB access
      * @param {*} dao 
      */
     constructor(dao) {
-        this.dao = dao
+        super(dao)
         this.SELECT_STATEMENT = `SELECT * FROM publishers`
     }
 
@@ -88,7 +38,7 @@ class PublisherRepository {
      */
     getFiltered(query) {
 
-        let query1 = this.SELECT_STATEMENT + buildFilteringWhereClause(query) + buildSortingOrderBy(query, "name")
+        let query1 = this.SELECT_STATEMENT + this.buildFilteringWhereClause(query) + this.buildSortingOrderBy(query, "name")
 
         return this.dao.all(query1)
     }
