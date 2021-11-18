bca5e898fb2cabfd07623a9128c64478b48be5b1
William Hendrick <william.hendrick@gmail.com>
2021-11-17 20:49:00 -0600

    refactor developer

--- controllers/developer_repository.js
+++ controllers/developer_repository.js
@@ -11,30 +11,9 @@ class DeveloperRepository extends Repository {
     constructor(dao) {
         super(dao)
         this.SELECT_STATEMENT = `SELECT * FROM developers`
+        this.PRIMARY_KEY = `name`
+        this.DEFAULT_SORT = `name`
     }
-
-    /**
-     * This method will fetch a single record from the database for the provide
-     * publisher name.
-     * @param {String} name Id for the record to select
-     * @returns result from the publisher selection - one record for the publisher entry
-     */
-    getById(name) {
-        return this.dao.get(this.SELECT_STATEMENT + ` WHERE name = ?`, [id])
-    }
-
-    /**
-     * 
-     * @param {*} query Filtering criteria in the embedded format - (?field=criteria) as well as sorting criteria
-     * @returns 
-     */
-    getFiltered(query) {
-
-        let query1 = this.SELECT_STATEMENT + this.buildFilteringWhereClause(query) + this.buildSortingOrderBy(query, "name")
-
-        return this.dao.all(query1)
-    }
-
 }
 
 module.exports = DeveloperRepository;
