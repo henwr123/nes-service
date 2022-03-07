65207db1543dfc73369f2dcb54bf97d4eb26325c
William Hendrick <william.hendrick@gmail.com>
2022-03-06 19:39:25 -0600

    Setup mocha testing and first test

---
+++ tests/e2e/test.spec.js
@@ -0,0 +1,40 @@
+const { italic } = require("colors");
+const request = require("supertest");
+const expect = require("chai").expect;
+
+describe("Testing the API", () => {
+
+    const baseUrl = 'http://localhost:4321'
+
+    const systemId = 'NES-NTSC'
+
+    it("should be able to get list of systems", (done) => {
+        request(baseUrl)
+            .get('/systems')
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .end((err, res) => {
+                expect(res.statusCode).to.be.equal(200)
+                expect(res.body.count).to.be.equal(5)
+                if(err) {
+                    throw err
+                }
+                done()
+            })
+    })
+
+    it("should be able to get a system", (done) => {
+        request(baseUrl)
+            .get('/systems/' + systemId)
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .end((err, res) => {
+                expect(res.statusCode).to.be.equal(200)
+                if(err) {
+                    throw err
+                }
+                done()
+            })
+    })
+
+});
