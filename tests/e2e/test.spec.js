5772af458d7b1e85434ff1e5f0bc41ff5499385d
William Hendrick <william.hendrick@gmail.com>
2022-03-06 21:59:38 -0600

    modularize the tests so far

--- tests/e2e/test.spec.js
+++ tests/e2e/test.spec.js
@@ -1,43 +1,115 @@
-const { italic } = require("colors");
-const request = require("supertest");
-const expect = require("chai").expect;
+const { italic } = require("colors")
+const request = require("supertest")
+const expect = require("chai").expect
+const assert = require('chai').assert
 
-describe("Testing the API", () => {
+const baseUrl = 'http://localhost:4321'
 
-    const baseUrl = 'http://localhost:4321'
+
+const capitalize = (s) => {
+    if (typeof s !== 'string') return ''
+    return s.charAt(0).toUpperCase() + s.slice(1)
+  }
+
+const runTestList = (entity, listCount, listFirstItem) => {
+
+    it(`${capitalize(entity)} - should be able to get list`, (done) => {
+        request(baseUrl)
+            .get('/' + entity)
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .end((err, res) => {
+                expect(res.statusCode).to.be.equal(200)
+                expect(res.body.count).to.be.equal(listCount)
+                expect(res.body.results[0].name).to.be.equal(listFirstItem)
+                if(err) {
+                    throw err
+                }
+                done()
+            })
+    })
+}
+
+/**
+ * Test to get a specific item
+ * @param {string} entity name of the entity
+ * @param {string} id item to get
+ */
+const runTestGetItem = (entity, id) => {
+
+    it(`${capitalize(entity)} - should be able to get an item`, (done) => {
+        request(baseUrl)
+            .get('/' + entity + '/' + id)
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .end((err, res) => {
+                expect(res.statusCode).to.be.equal(200)
+                expect(res.body.name).to.be.equal(id)
+                if(err) {
+                    throw err
+                }
+                done()
+            })
+    })
+
+}
+
+describe("Testing the nes-service API - Get Data", () => {
+
 
     /// Systems ///////////////////////////////////////////////////////////////
 
     const systemId = 'NES-NTSC'
+    const systemFirstItem = 'Famicom'
 
-    it("should be able to get list of systems", (done) => {
-        request(baseUrl)
-            .get('/systems')
-            .set('Accept', 'application/json')
-            .set('Content-Type', 'application/json')
-            .end((err, res) => {
-                expect(res.statusCode).to.be.equal(200)
-                expect(res.body.count).to.be.equal(5)
-                expect(res.body.results[0].name).to.be.equal('Famicom')
-                if(err) {
-                    throw err
-                }
-                done()
-            })
+    context('System Tests', () => {
+        runTestList('systems', 5, systemFirstItem)
+        runTestGetItem('systems', systemId)
     })
 
-    it("should be able to get a system", (done) => {
-        request(baseUrl)
-            .get('/systems/' + systemId)
-            .set('Accept', 'application/json')
-            .set('Content-Type', 'application/json')
-            .end((err, res) => {
-                expect(res.statusCode).to.be.equal(200)
-                if(err) {
-                    throw err
-                }
-                done()
-            })
+
+    /// Boards ///////////////////////////////////////////////////////////////
+
+    const boardId = 'HVC-CNROM'
+    const boardFirstItem = 'ACCLAIM-AOROM'
+
+    context('Board Tests', () => {
+        runTestList('boards', 67, boardFirstItem)
+        runTestGetItem('boards', boardId)
+
+
+        it("Boards - should be able to filter by value", (done) => {
+            request(baseUrl)
+                .get('/boards/?name=' + boardId)
+                .set('Accept', 'application/json')
+                .set('Content-Type', 'application/json')
+                .end((err, res) => {
+                    expect(res.statusCode).to.be.equal(200)
+                    expect(res.body.count).to.be.equal(1)
+                    expect(res.body.results[0].name).to.be.equal(boardId)
+                    if(err) {
+                        throw err
+                    }
+                    done()
+                })
+        })
+
+        it("Boards - should be able to filter by pattern", (done) => {
+            request(baseUrl)
+                .get('/boards/?name=%An%')
+                .set('Accept', 'application/json')
+                .set('Content-Type', 'application/json')
+                .end((err, res) => {
+                    expect(res.statusCode).to.be.equal(200)
+                    expect(res.body.count).to.be.equal(2)
+                    //expect(res.body.results).to.match(/an/gmi)
+                    if(err) {
+                        throw err
+                    }
+                    done()
+                })
+        })
+
     })
 
-});
+})
